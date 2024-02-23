9e2352c45af11a85d4d528a69e638fde85e0e6bf
Gustavo Araujo <gustavo.sousa12@gmail.com>
2024-02-23 13:46:21 -0300

    Criação da página Home e Datails

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,13 +1,45 @@
-import { Container } from './style';
+import { Container, Links, Content } from './style';
 
+import { Header } from '../../components/Header';
+import { Section } from '../../components/Section';
+import { ButtonText } from '../../components/ButtonText';
 import { Button } from '../../components/Button';
+import { Tag } from '../../components/Tag';
 
 export function Details(){
   return(
       <Container>
-        <h1>Gustavo Araujo</h1>
-        <Button title="Cadastrar" loading/>
-        <Button title="Excluir"/>
+        <Header/>
+        <main>
+          <Content>          
+            <ButtonText title="Excluir nota" />
+
+            <h1>Introdução ao React</h1>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Quam sed quia porro commodi, iusto magni aspernatur obcaecati cum, 
+              esse nam modi eum, veritatis atque iste quo temporibus quis explicabo 
+              rem.</p>
+
+            <Section title="Links úteis">
+              <Links>
+                <li>
+                  <a href="https://github.com/FalconXD">https://github.com/FalconXD</a>
+                </li>
+                <li>
+                <a href="https://github.com/FalconXD">https://github.com/FalconXD</a>
+                </li>
+              </Links>
+            </Section>
+
+            <Section title="Marcadores">
+              <Tag title="Express" />
+              <Tag title="Node" />
+            </Section>
+
+            <Button title="Voltar"/>
+          </Content>
+        </main>
       </Container>
   )
 }
