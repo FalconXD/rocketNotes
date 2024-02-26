ac5b883b7fec13c3fbe408fd693c1873559ac5ca
Gustavo Araujo <gustavo.sousa12@gmail.com>
2024-02-26 20:02:27 -0300

    Criação dos arquivos de rotas

--- src/pages/SignIn/style.js
+++ src/pages/SignIn/style.js
@@ -24,7 +24,7 @@ export const Form = styled.form`
 
   > h2{
     font-size: 24px;
-    margin-top: 48px 0;
+    margin: 48px 0;
   }
 
   > p{
@@ -42,5 +42,5 @@ export const Background = styled.div`
   flex: 1;
   background: url(${backgroundImg}) no-repeat center center;
   background-size: cover;
-
+  opacity: 0.5;
 `;
